98ce0afeeaaa3977b79d29b58f2cb0deb7d5d7eb
Athlon1600 <Athlon2400@gmail.com>
2023-02-14 16:45:42 -0600

    added Caddyfile

--- www/index.php
+++ www/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>AZ Environment variables 1.04</title>
-<meta name="keywords" content="proxyjudge, proxy judge, azenv proxy judge, azenv.php" />
+    <title>AZ Environment variables 1.04</title>
+    <meta name="keywords" content="proxyjudge, proxy judge, azenv proxy judge, azenv.php" />
 </head>
 <body>
 <h1>PHP Proxy Judge</h1>
@@ -30,19 +30,19 @@
 Meta-variables with names beginning with "HTTP_" contain values read from the client request header fields, if the protocol used is HTTP.
 The HTTP header field name is converted to upper case, has all occurrences of "-" replaced with "_" and has "HTTP_" prepended to give the meta-variable name.
 
-*/ 
+*/
 ##########################################################################
 
 
 if(isset($_SERVER['HTTP_X_CUSTOM_REAL_IP'])){
-	$_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_CUSTOM_REAL_IP'];
-	unset($_SERVER['HTTP_X_CUSTOM_REAL_IP']);
+    $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_CUSTOM_REAL_IP'];
+    unset($_SERVER['HTTP_X_CUSTOM_REAL_IP']);
 }
 
-   
+
 foreach ($_SERVER as $header => $value )
 { if 	(strpos($header , 'REMOTE')!== false || strpos($header , 'HTTP')!== false ||
-	strpos($header , 'REQUEST')!== false) {echo $header.' = '.$value."\n"; } }
+    strpos($header , 'REQUEST')!== false) {echo $header.' = '.$value."\n"; } }
 ?>
 </pre>
 </body>
